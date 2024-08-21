7fc555953ba49053cc86be92c76d5f6f5fefa63f
PabloMenendez06 <pmenendez-2020332@kinal.edu.gt>
2024-08-20 18:55:35 -0600

    Controllers actualizados

--- src/main/java/com/pabomenendez/webapp/biblioteca/controller/EmpleadoController.java
+++ src/main/java/com/pabomenendez/webapp/biblioteca/controller/EmpleadoController.java
@@ -8,6 +8,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,8 @@ import com.pabomenendez.webapp.biblioteca.service.EmpleadoService;
 @Controller
 @RestController
 @RequestMapping("")
+@CrossOrigin(value = "http://127.0.0.1:5500")
+
 public class EmpleadoController {
 
     @Autowired
